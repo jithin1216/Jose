af156857a77a0a2233db9f6368affd6282d4f910
jithin <jithinpjose93@gmail.com>
2020-07-04 23:52:45 -0700

    First commit of JoseOne

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,8 +2,9 @@ import com.example.*
 class BootStrap {
 
     def init = { servletContext ->
-		def person=new Person(name : "Tony",age : "26").save(flush: true) 
-    }
-    def destroy = {
+		def person=new Person(name : "Tony",age : "26").save(flush: true)
+		def person1=new Person(name : "Jithin",age : "28").save(flush: true)
+    }  
+    def destroy = {            
     }
 }
